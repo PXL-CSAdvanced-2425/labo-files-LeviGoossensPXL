2d206b8690f769ec58fc649bb8daec842058a7e2
Goossens Levi <Levi.Goossens@student.pxl.be>
2025-03-04 09:30:45 +0100

    choose file to save or to read

--- LaboFiles/LaboFiles.WPF/MainWindow.xaml.cs
+++ LaboFiles/LaboFiles.WPF/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace LaboFiles.WPF;
 
@@ -39,26 +40,49 @@ public partial class MainWindow : Window
 
     private void saveFileButton_Click(object sender, RoutedEventArgs e)
     {
-        using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
-        using (StreamWriter sw = new StreamWriter(fs))
+        SaveFileDialog sfd = new SaveFileDialog()
         {
-            for (int i = 0; i < firstNameListBox.Items.Count; i++)
+            Filter = "Alle bestanden|*.*|Tekstbestanden|*.txt|CSV bestanden|*.csv",
+            FilterIndex = 2,
+            Title = "Opslaan Personen Info",
+            OverwritePrompt = true,
+            AddExtension = true,
+            FileName = fileName,
+            InitialDirectory = Environment.CurrentDirectory
+        };
+        if (sfd.ShowDialog() == true)
+        {
+            using (FileStream fs = new FileStream(sfd.FileName, FileMode.Create, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
             {
-                sw.WriteLine($"{firstNameListBox.Items[i]}{sepChar}{lastNameListBox.Items[i]}");
+                for (int i = 0; i < firstNameListBox.Items.Count; i++)
+                {
+                    sw.WriteLine($"{firstNameListBox.Items[i]}{sepChar}{lastNameListBox.Items[i]}");
+                }
             }
         }
     }
 
     private void readFileButton_Click(object sender, RoutedEventArgs e)
     {
-        using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-        using (StreamReader sr = new StreamReader(fs))
+        OpenFileDialog ofd = new OpenFileDialog()
         {
-            while (!sr.EndOfStream)
+            Filter = "Alle bestanden|*.*|Tekstbestanden|*.txt|CSV bestanden|*.csv",
+            FilterIndex = 2,
+            Title = "Open Personen Info",
+            InitialDirectory = Environment.CurrentDirectory
+        };
+        if (ofd.ShowDialog() == true)
+        {
+            using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open, FileAccess.Read))
+            using (StreamReader sr = new StreamReader(fs))
             {
-                string[] contents = sr.ReadLine().Split(sepChar);
-                firstNameListBox.Items.Add(contents[0]);
-                lastNameListBox.Items.Add(contents[1]);
+                while (!sr.EndOfStream)
+                {
+                    string[] contents = sr.ReadLine().Split(sepChar);
+                    firstNameListBox.Items.Add(contents[0]);
+                    lastNameListBox.Items.Add(contents[1]);
+                }
             }
         }
     }
